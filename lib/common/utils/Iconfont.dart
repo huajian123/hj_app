eb51b04164952fb8783ac75460317683173423ae
huajian <287643967@qq.com>
2020-03-30 14:43:39 +0800

    添加自定义图标

---
+++ lib/common/utils/Iconfont.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Iconfont {
+    // iconName: qq
+  static const qq = IconData(
+    0xe654,
+    fontFamily: 'Iconfont',
+    matchTextDirection: true,
+  );
+
+
+}
