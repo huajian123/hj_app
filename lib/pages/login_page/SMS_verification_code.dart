b454d1da8a175d36cea39c849395987699030f73
huajian <287643967@qq.com>
2020-04-01 14:46:23 +0800

    短信验证码登录页面提交

--- lib/pages/login_page/SMS_verification_code.dart
+++ lib/pages/login_page/SMS_verification_code.dart
@@ -11,7 +11,51 @@ class VerificationCode extends StatefulWidget {
 class _VerificationCodeState extends State<VerificationCode> {
   /// Control the input text field.
   TextEditingController _pinEditingController = TextEditingController();
-  var _returnTime = false;
+  var _isStartCountdownTime = false; // 是否开始验证码计时
+  Timer _timer;
+  int _countdownTime = 5; // 验证码时间
+  var _isReGetCode = false; //是否重新获取验证码
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (_timer != null) {
+      _timer.cancel();
+      _timer = null;
+    }
+  }
+
+  void startCountdownTimer() {
+    setState(() {
+      _isReGetCode = false;
+    });
+    const oneSec = const Duration(seconds: 1);
+    var callback = (timer) {
+      setState(() {
+        if (_countdownTime < 1) {
+          _timer.cancel();
+          _timer = null;
+          _isStartCountdownTime = false;
+          _isReGetCode = true;
+          _countdownTime = 5;
+        } else {
+          _countdownTime = _countdownTime - 1;
+          _isStartCountdownTime = true;
+          _isReGetCode = false;
+        }
+      });
+    };
+
+    _timer = Timer.periodic(oneSec, callback);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _isStartCountdownTime = true;
+    startCountdownTimer();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,20 +86,17 @@ class _VerificationCodeState extends State<VerificationCode> {
               ),
               PinInputTextField(
                 onChanged: (value) {
-                  print(value);
-                  if (value.length == 4) {
-                    print("输入完成");
-                  } else {
-                    print("等等");
-                  }
+                  if (value.length == 4 && !_isStartCountdownTime) {}
                 },
                 decoration: UnderlineDecoration(
-                    gapSpace: duSetWidth(50),
-                    lineHeight: 1,
-                    color: ColorsUtil.hexColor(0xcccccc),
-                    textStyle: TextStyle(
-                        color: ColorsUtil.hexColor(0x222222),
-                        fontSize: duSetFontSize(54))),
+                  gapSpace: duSetWidth(50),
+                  lineHeight: 1,
+                  color: ColorsUtil.hexColor(0xcccccc),
+                  textStyle: TextStyle(
+                    color: ColorsUtil.hexColor(0x222222),
+                    fontSize: duSetFontSize(54),
+                  ),
+                ),
                 autoFocus: false,
                 pinLength: 4,
                 controller: _pinEditingController,
@@ -63,11 +104,28 @@ class _VerificationCodeState extends State<VerificationCode> {
               SizedBox(
                 height: duSetHeight(42),
               ),
-              Text(
-                "45秒后重新获取验证码",
-                style: TextStyle(
-                    color: ColorsUtil.hexColor(0x999999),
-                    fontSize: duSetFontSize(28)),
+              Visibility(
+                visible: _isReGetCode,
+                child: GestureDetector(
+                  child: Text(
+                    "重新获取验证码",
+                    style: TextStyle(
+                      fontFamily: "Avenir",
+                      color: Theme.of(context).primaryColor,
+                      fontSize: duSetFontSize(28),
+                    ),
+                  ),
+                  onTap: () => startCountdownTimer(),
+                ),
+              ),
+              Visibility(
+                visible: !_isReGetCode,
+                child: Text(
+                  "$_countdownTime秒后重新获取验证码",
+                  style: TextStyle(
+                      color: ColorsUtil.hexColor(0x999999),
+                      fontSize: duSetFontSize(28)),
+                ),
               ),
             ],
           ),
